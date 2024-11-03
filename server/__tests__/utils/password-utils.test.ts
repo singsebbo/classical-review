e0d37a105516e0be08c7f419dfc7d482948654fe
singsebbo <sebbolam@gmail.com>
2024-11-02 22:06:54 -0700

    Add and test password hash utility

---
+++ server/__tests__/utils/password-utils.test.ts
@@ -0,0 +1,7 @@
+import { hashPassword } from "../../src/utils/password-utils";
+
+test("should successfully hash a password", async (): Promise<void> => {
+  const password = "Password123!";
+  const hashedPassword: string = await hashPassword(password);
+  expect(hashedPassword).not.toBe(password);
+});
--- server/src/controllers/account-controller.ts
+++ server/src/controllers/account-controller.ts
@@ -1,4 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+import UserModel from "../models/user-model";
+import { User } from "../interfaces/entities";
+import { RegistrationData } from "../interfaces/request-interfaces";
+import { hashPassword } from "../utils/password-utils";
 
 /**
  * Registers a new user.
@@ -13,7 +17,18 @@ export async function registerUser(
   next: NextFunction
 ): Promise<void> {
   try {
-    res.status(201).json({ message: "User has successfully registered" });
+    const { username, email, password }: RegistrationData = req.body;
+    const passwordHash: string = await hashPassword(password);
+    const user: User = await UserModel.createUser(
+      username,
+      email,
+      passwordHash
+    );
+    // Send JWT email
+    res.status(201).json({
+      success: true,
+      message: "User has been successfully registered",
+    });
   } catch (error: unknown) {
     next(error);
   }
