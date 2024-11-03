bf177bb2dd8f7384a019f1a780938d5f032b5eaf
singsebbo <sebbolam@gmail.com>
2024-11-02 22:25:39 -0700

    Add comments to util functions

--- server/src/utils/password-utils.ts
+++ server/src/utils/password-utils.ts
@@ -2,6 +2,11 @@ import bcrypt from "bcrypt";
 
 const SALT_ROUNDS = 10;
 
+/**
+ * Hashes and returns the hashed password.
+ * @param {string} password - The password to hash.
+ * @returns The hashed password.
+ */
 export async function hashPassword(password: string): Promise<string> {
   return await bcrypt.hash(password, SALT_ROUNDS);
 }
--- server/src/utils/token-utils.ts
+++ server/src/utils/token-utils.ts
@@ -1,6 +1,11 @@
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
 
+/**
+ * Creates a jwt token meant for email verification.
+ * @param {string} userId - User ID to incorporate in jwt.
+ * @returns JSON web token that expires in 24 hours.
+ */
 export function createEmailVerificationToken(userId: string): string {
   const token: string = jwt.sign({ userId }, JWT_SECRET, { expiresIn: "24h" });
   return token;
