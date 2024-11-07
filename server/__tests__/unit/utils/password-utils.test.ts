ce1b91e745ec3bf073469b932e756b018ffd8d0b
singsebbo <sebbolam@gmail.com>
2024-11-06 22:07:38 -0800

    Test and implement isValidPassword

--- server/__tests__/unit/utils/password-utils.test.ts
+++ server/__tests__/unit/utils/password-utils.test.ts
@@ -1,7 +1,16 @@
-import { hashPassword } from "../../../src/utils/password-utils";
+import {
+  hashPassword,
+  isValidPassword,
+} from "../../../src/utils/password-utils";
 
 test("should successfully hash a password", async (): Promise<void> => {
   const password = "Password123!";
   const hashedPassword: string = await hashPassword(password);
   expect(hashedPassword).not.toBe(password);
+});
+
+test("should successfully validate password", async (): Promise<void> => {
+  const password = "unhashedPassword99!";
+  const hashedPassword: string = await hashPassword(password);
+  expect(isValidPassword(password, hashedPassword)).resolves.toBe(true);
 });
--- server/src/utils/password-utils.ts
+++ server/src/utils/password-utils.ts
@@ -10,3 +10,16 @@ const SALT_ROUNDS = 10;
 export async function hashPassword(password: string): Promise<string> {
   return await bcrypt.hash(password, SALT_ROUNDS);
 }
+
+/**
+ * Checks if the unhashed password matches the hashed password.
+ * @param {string} password - The unhashed password.
+ * @param {string} passwordHash - The hashed password.
+ * @returns A promise that resolves to a boolean representing the result of the comparison.
+ */
+export async function isValidPassword(
+  password: string,
+  passwordHash: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, passwordHash);
+}
