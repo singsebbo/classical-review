e1d7f4dc9bdee107cf26a2c57eac9895ef7c7eff
singsebbo <sebbolam@gmail.com>
2024-11-14 20:23:47 -0800

    Create validateSearchTerm

--- server/src/validators/search-validators.ts
+++ server/src/validators/search-validators.ts
@@ -1,10 +1,15 @@
 import { query, ValidationChain } from "express-validator";
 
 function validateSearchTerm(): ValidationChain {
-  return query("term");
-  /**
-   * @todo
-   */
+  return query("term")
+    .exists()
+    .withMessage("Search term must not be empty.")
+    .isString()
+    .withMessage("Search term must be a string.")
+    .isLength({ min: 1, max: 50 })
+    .withMessage("Search term must be between 1 and 50 characters long.")
+    .escape()
+    .trim();
 }
 
 /** Validates Express request for GET /api/search/composers */
