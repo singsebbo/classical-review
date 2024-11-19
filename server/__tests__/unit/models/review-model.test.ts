a704cab5f6c4255a29066a2bac3f7570044fcf9c
singsebbo <sebbolam@gmail.com>
2024-11-18 22:51:34 -0800

    Create getCompositionReviews tests

---
+++ server/__tests__/unit/models/review-model.test.ts
@@ -0,0 +1,27 @@
+import database from "../../../src/database";
+import ModelError from "../../../src/errors/model-error";
+import ReviewModel from "../../../src/models/review-model";
+
+jest.mock("../../../src/database");
+
+describe("getCompositionReviews tests", (): void => {
+  const compositionId = "alksdjf293ur";
+  test("should fail if the query fails", async (): Promise<void> => {
+    const mockError: Error = new Error("Database connection failed");
+    (database.query as jest.Mock).mockRejectedValue(mockError);
+    await expect(
+      ReviewModel.getCompositionReviews(compositionId)
+    ).rejects.toThrow(
+      new ModelError("Database error while getting composition reviews.", 500)
+    );
+  });
+  test("should successfully get composition reviews", async (): Promise<void> => {
+    const mockResult: { rows: any[] } = {
+      rows: [{ review_id: "1" }, { review_id: "2" }],
+    };
+    (database.query as jest.Mock).mockResolvedValue(mockResult);
+    await expect(
+      ReviewModel.getCompositionReviews(compositionId)
+    ).resolves.toEqual(mockResult.rows);
+  });
+});
