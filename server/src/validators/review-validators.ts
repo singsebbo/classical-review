56e8e46ad765ab308f27593cd1e56d8d7a19d234
singsebbo <sebbolam@gmail.com>
2024-12-03 18:05:47 -0800

    Create validator for review

--- server/src/validators/review-validators.ts
+++ server/src/validators/review-validators.ts
@@ -1,14 +1,62 @@
-import { ValidationChain } from "express-validator";
+import { body, ValidationChain } from "express-validator";
+import CompositionModel from "../models/composition-model";
+import profanities from "../utils/profanities";
+
+/**
+ * Checks if text contains any profanity.
+ * @param {string} text - Text to check.
+ * @returns True if there is no profanity, false otherwise.
+ */
+function containsNoProfanity(text: string): boolean {
+  text = text.toLowerCase();
+  return !profanities.some((word: string): boolean => text.includes(word));
+}
+
+function validateComposition(): ValidationChain {
+  return body("compositionId")
+    .exists()
+    .withMessage("Composition ID must exist.")
+    .bail()
+    .custom(async (compositionId: string): Promise<void> => {
+      const compositionExists: boolean =
+        await CompositionModel.compositionExists(compositionId);
+      if (!compositionExists) {
+        throw new Error("Composition does not exist.");
+      }
+    });
+}
+
+function validateRating(): ValidationChain {
+  return body("rating")
+    .exists()
+    .withMessage("Rating must exist.")
+    .bail()
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Rating must be between 1 and 5.")
+    .bail();
+}
+
+function validateComment(): ValidationChain {
+  return body("comment")
+    .optional()
+    .trim()
+    .isAlphanumeric("en-US")
+    .withMessage(
+      "Comment must be alphanumeric and follow 'en-US' language code."
+    )
+    .bail()
+    .isLength({ min: 10, max: 1000 })
+    .withMessage("Comment must be between 10 and 1000 character.")
+    .bail()
+    .custom((comment: string): boolean => {
+      return containsNoProfanity(comment);
+    })
+    .withMessage("Comment must not contain profanity.");
+}
 
 /** Validates Express request for POST /api/review/make-review */
 export const makeReviewValidator: ValidationChain[] = [
-  /**
-   * @todo Check if compositionId field exists
-   * @todo Check if compositionId exists in the database
-   * @todo Check if rating field exists
-   * @todo Check if rating is between 1 and 5
-   * @todo Trim the comment of spaces on the left and right
-   * @todo If comment exists, check if it is in the character limit
-   * @todo If comment exists, check for profanity
-   */
+  validateComposition(),
+  validateRating(),
+  validateComment(),
 ];
