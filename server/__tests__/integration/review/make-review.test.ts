255d9efc1c908e0a1423effceaf27e7b35d5515b
singsebbo <sebbolam@gmail.com>
2024-12-27 02:22:29 -0800

    Finish writing validation chain tests

--- server/__tests__/integration/review/make-review.test.ts
+++ server/__tests__/integration/review/make-review.test.ts
@@ -1,12 +1,20 @@
 import request, { Response as SupertestResponse } from "supertest";
 import jwt from "jsonwebtoken";
 import app from "../../../src/app";
-import { createEmailVerificationToken } from "../../../src/utils/token-utils";
+import {
+  createAccessToken,
+  createEmailVerificationToken,
+} from "../../../src/utils/token-utils";
+import CompositionModel from "../../../src/models/composition-model";
+
+jest.mock("../../../src/models/composition-model");
 
 let consoleErrorSpy: jest.SpyInstance;
 
-beforeAll((): void => {
-  consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(jest.fn());
+beforeEach((): void => {
+  consoleErrorSpy = jest
+    .spyOn(console, "error")
+    .mockImplementation((): void => {});
 });
 
 afterEach((): void => {
@@ -14,6 +22,8 @@ afterEach((): void => {
 });
 
 describe("POST /api/review/make-review tests", (): void => {
+  const args: [string, number, string] = ["compositionId", 4, "comment"];
+  const bearerToken: string = createAccessToken("userId1");
   describe("Validation tests", (): void => {
     describe("Bearer token tests", (): void => {
       test("should fail if authorization header does not exist", async (): Promise<void> => {
@@ -72,6 +82,214 @@ describe("POST /api/review/make-review tests", (): void => {
           "message",
           "Authentication error encountered while making a review"
         );
+      });
+    });
+    describe("Composition tests", (): void => {
+      test("should fail if compositionId does not exist", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            rating: 4,
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Composition ID must exist.",
+          },
+        ]);
+      });
+      test("should fail if composition does not exist", async (): Promise<void> => {
+        (CompositionModel.compositionExists as jest.Mock).mockResolvedValue(
+          false
+        );
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 4,
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Composition does not exist.",
+          },
+        ]);
+      });
+    });
+    describe("Rating tests", (): void => {
+      beforeAll((): void => {
+        (CompositionModel.compositionExists as jest.Mock).mockResolvedValue(
+          true
+        );
+      });
+      test("should fail if rating does not exist", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Rating must exist.",
+          },
+        ]);
+      });
+      test("should fail if rating is not an integer", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 4.2,
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Rating must be an integer between 1 and 5.",
+          },
+        ]);
+      });
+      test("should fail if rating is out of range", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 6,
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Rating must be an integer between 1 and 5.",
+          },
+        ]);
+      });
+    });
+    describe("comment tests", (): void => {
+      beforeAll((): void => {
+        (CompositionModel.compositionExists as jest.Mock).mockResolvedValue(
+          true
+        );
+      });
+      test("should fail if comment is not following en-US code", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 5,
+            comment: "ñoasdfajsdfpojasdjfalkfjkl",
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message:
+              "Comment must be alphanumeric and follow 'en-US' language code.",
+          },
+        ]);
+      });
+      test("should fail if comment is contains special characters", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 5,
+            comment: "$%^&*()(*&^%^&*()",
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message:
+              "Comment must be alphanumeric and follow 'en-US' language code.",
+          },
+        ]);
+      });
+      test("should fail if comment is less than 10 characters", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 5,
+            comment: "af",
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Comment must be between 10 and 1000 characters.",
+          },
+        ]);
+      });
+      test("should fail if comment is more than 1000 characters", async (): Promise<void> => {
+        let longComment: string = "";
+        for (let i = 0; i < 1000; i++) {
+          longComment += "adlkaskfjsk";
+        }
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 5,
+            comment: longComment,
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Comment must be between 10 and 1000 characters.",
+          },
+        ]);
+      });
+      test("should fail if comment contains profanity", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .post("/api/review/make-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            compositionId: "aslkfjaklasdf",
+            rating: 5,
+            comment: "shit shit shit shit",
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Comment must not contain profanity.",
+          },
+        ]);
       });
     });
   });
