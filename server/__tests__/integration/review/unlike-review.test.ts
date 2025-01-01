8addf5914be3b88db35fe23a570bf8f1cc9bdb58
singsebbo <sebbolam@gmail.com>
2025-01-01 14:57:46 -0800

    Create unlike-review validation tests

---
+++ server/__tests__/integration/review/unlike-review.test.ts
@@ -0,0 +1,143 @@
+import request, { Response as SupertestResponse } from "supertest";
+import jwt from "jsonwebtoken";
+import app from "../../../src/app";
+import {
+  createAccessToken,
+  createEmailVerificationToken,
+} from "../../../src/utils/token-utils";
+import ReviewModel from "../../../src/models/review-model";
+import LikedReviewsModel from "../../../src/models/liked-reviews-model";
+
+jest.mock("../../../src/models/review-model");
+jest.mock("../../../src/models/liked-reviews-model");
+
+let consoleErrorSpy: jest.SpyInstance;
+
+beforeEach((): void => {
+  consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(jest.fn());
+});
+
+afterEach((): void => {
+  consoleErrorSpy.mockRestore();
+});
+
+describe("delete /api/review/unlike-review test", (): void => {
+  const bearerToken: string = createAccessToken("dn4up8nps89f");
+  const reviewId = "asifh32908dasihf0apwfp9";
+  describe("Validation tests", (): void => {
+    describe("Bearer token tests", (): void => {
+      test("should fail if authorization header does not exist", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app).delete(
+          "/api/review/unlike-review"
+        );
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty(
+          "message",
+          "Authentication error encountered while unliking a review"
+        );
+      });
+      test("should fail if authorization header is not a bearer", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .delete("/api/review/unlike-review")
+          .set("Authorization", "Bear jsadklfjq9w.asdf3q9pfi.lasdkjfapsf");
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty(
+          "message",
+          "Authentication error encountered while unliking a review"
+        );
+      });
+      test("should fail if authorization header does not contain a token", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .delete("/api/review/unlike-review")
+          .set("Authorization", "Bearer ");
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty(
+          "message",
+          "Authentication error encountered while unliking a review"
+        );
+      });
+      test("should fail if token is signed incorrectly", async (): Promise<void> => {
+        const token: string = jwt.sign("token", "key");
+        const response: SupertestResponse = await request(app)
+          .delete("/api/review/unlike-review")
+          .set("Authorization", `Bearer ${token}`);
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty(
+          "message",
+          "Authentication error encountered while unliking a review"
+        );
+      });
+      test("should fail if token is not for the purpose of access", async (): Promise<void> => {
+        const token: string = createEmailVerificationToken("askdjfas");
+        const response: SupertestResponse = await request(app)
+          .delete("/api/review/unlike-review")
+          .set("Authorization", `Bearer ${token}`);
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty(
+          "message",
+          "Authentication error encountered while unliking a review"
+        );
+      });
+    });
+    describe("unlikeReview validator tests", (): void => {
+      test("should fail if reviewId does not exist", async (): Promise<void> => {
+        const response: SupertestResponse = await request(app)
+          .delete("/api/review/unlike-review")
+          .set("Authorization", `Bearer ${bearerToken}`);
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Review ID must exist.",
+          },
+        ]);
+      });
+      test("should fail if review does not exist", async (): Promise<void> => {
+        (ReviewModel.getReview as jest.Mock).mockResolvedValue(null);
+        const response: SupertestResponse = await request(app)
+          .delete("/api/review/unlike-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            reviewId: reviewId,
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Review does not exist.",
+          },
+        ]);
+      });
+      test("should fail if review is not liked", async (): Promise<void> => {
+        (ReviewModel.getReview as jest.Mock).mockResolvedValue({});
+        (LikedReviewsModel.getLikedReviews as jest.Mock).mockResolvedValue([
+          { review_id: "otherReviewId" },
+        ]);
+        const response: SupertestResponse = await request(app)
+          .delete("/api/review/unlike-review")
+          .set("Authorization", `Bearer ${bearerToken}`)
+          .send({
+            reviewId: reviewId,
+          });
+        expect(consoleErrorSpy).toHaveBeenCalled();
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("success", false);
+        expect(response.body).toHaveProperty("message", [
+          {
+            type: "field",
+            message: "Review is not liked.",
+          },
+        ]);
+      });
+    });
+  });
+});
