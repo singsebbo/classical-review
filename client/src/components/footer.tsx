b8d39ab034889da390de797efdb7f331f8bda110
singsebbo <sebbolam@gmail.com>
2025-02-06 14:33:32 -0800

    Create basic footer

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,8 +1,14 @@
+import github from "../assets/github.svg";
+
 function Footer(): JSX.Element {
   return (
     <>
-      <footer>
-        Footer
+      <footer className="flex items-center justify-center p-2 sm:p-4 md:px-8 md:py-4">
+        <div className="flex items-center">
+          <a href="https://github.com/singsebbo/classical-review" target="_blank">
+            <img src={github} alt="github logo" className="h-4 w-4 sm:h-6 sm:w-6 md:h-8 md:w-8" />
+          </a>
+        </div>
       </footer>
     </>
   )
