f18807cea4e20286d607c032cf41a9f3261f1bb8
singsebbo <sebbolam@gmail.com>
2025-06-03 21:14:53 -0700

    Add navigation to home page in header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -6,10 +6,12 @@ function Header(): JSX.Element {
   return (
     <>
       <header className="flex items-center justify-between p-2 sm:p-4 md:px-8 md:py-4">
-        <div className="flex items-center">
-          <img src={logo} alt="logo" className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14" />
-          <span className="transition ease-in-out duration-200 hover:text-sunset text-lg sm:text-2xl md:text-4xl">Classical Review</span>
-        </div>
+        <NavLink to="/">
+          <div className="flex items-center">
+            <img src={logo} alt="logo" className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14" />
+            <span className="transition ease-in-out duration-200 hover:text-sunset text-lg sm:text-2xl md:text-4xl">Classical Review</span>
+          </div>
+        </NavLink>
         <div>
           <NavLink to="/user">
             <img
